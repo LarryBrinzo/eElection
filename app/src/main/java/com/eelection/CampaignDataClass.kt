3f9cac579436db84c6b8e73573819f09020d7e27
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-06 09:17:40 +0800

    Delete app directory

--- app/src/main/java/com/eelection/CampaignDataClass.kt
+++
@@ -1,6 +0,0 @@
-package com.eelection
-
-data class CampaignDataClass(var Party_Name:String, var Video_URL:String, var Image:String, var Type:String,
-                             var Title:String,
-                             var CampaignNumber:String,
-                             var Time:String)
--- app/src/main/java/com/eelection/PostDataClass.kt
+++
@@ -1,7 +0,0 @@
-package com.eelection
-
-data class PostDataClass(var Username:String="", var UserImg:String="", var Designation:String="",var Party_Name:String="",
-                         var Video_Url:String="", var Image:String="", var Type:String="", var PostText:String="",
-                             var Likes:String="", var Comments:String="",
-                             var PostNumber:String="", var UserLike:String=""
-                             )
